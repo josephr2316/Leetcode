ed9686b5ed367f6b8fe0635a9ed0ee6c1c5af564
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2026-01-31 18:32:28 -0400

    1. Two Sum in C#

---
+++ C#/1. Two Sum.cs
@@ -0,0 +1,78 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+    //Brute Force Approach
+    //    int[] result = new int[2];
+    //     for (int i = 0; i < nums.Length; i++) {
+    //         for (int j = i + 1; j < nums.Length; j++) {
+    //             if (nums[i] + nums[j] == target) {
+    //                 result[0] = i;
+    //                 result[1] = j;
+    //                 return result;
+    //             }
+    //         }
+    //     }
+    //     return result;
+    // }
+
+    //Brute Force Approach
+    // for (int i = 0; i < nums.Length; i++){
+    //     for (int j = i + 1; j<nums.Length; j++){
+    //         if ((nums[i] + nums[j]) == target){
+    //              return new int[]{i,j};
+    //         }
+    //      }
+    // }
+    // return new int[]{};
+
+    //Sorting and Two Pointers Approach
+    //  (int num, int index)[] numbers = nums.Select((num, index) => (num, index)).ToArray();
+    // Array.Sort(numbers, (a, b) => a.num.CompareTo(b.num));
+
+    // int left = 0, right = nums.Length - 1;
+    // while(left < right) {
+    //     int sum = numbers[left].num + numbers[right].num;
+    //     if(sum == target) {
+    //         return new int[] { numbers[left].index, numbers[right].index };
+    //     } else if(sum < target) {
+    //         left++;
+    //     } else {
+    //         right--;
+    //     }
+    // }
+    // return new int[] { };
+  
+
+    //Hash Table Appoach
+    // Dictionary<int, int> map = new Dictionary<int, int>();
+    // for (int i = 0; i < nums.Length; i++){
+    //     int complement = target - nums[i];
+    //     if (map.ContainsKey(complement)){
+    //         return new int[]{map[complement], i};
+    //     }
+    //     if (!map.ContainsKey(nums[i])){
+    //         map.Add(nums[i], i);
+    //     }
+    // }
+    // return new int[]{};
+
+    // var map = new Dictionary<int, int>();
+    // for (int i = 0; i < nums.Length; i++)
+    //     if (map.TryGetValue(target - nums[i], out var j))
+    //         return new[] { j, i };
+    //     else
+    //         map[nums[i]] = i;
+
+    // return Array.Empty<int>();
+
+     Dictionary<int, int> map = new Dictionary<int, int>();
+
+    for(int i = 0; i < nums.Length; i++) {
+        int complement = target - nums[i];
+        if(map.ContainsKey(complement)) {
+            return new int[] { map[complement], i };
+        }
+        map[nums[i]] = i;
+    }
+    return new int[] { };
+  }
+}
